58e9a77bba0656d502e1204c6942345b02788dc9
prince-73ssan <prince532@outlook.com>
2017-08-22 21:38:05 +0400

    First commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+  Image,
+} from 'react-native';
